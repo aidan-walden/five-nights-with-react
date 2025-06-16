52c9d348e6e5d3e8eaf6ec524e416d53d56b9f30
Aidan Walden <44129920+aidan-walden@users.noreply.github.com>
2025-06-15 23:45:13 -0400

    Initial upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Warning from "./screens/Warning"
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <Warning />
+    </div>
+  )
+}
+
+export default App
---
+++ src/screens/Warning.tsx
@@ -0,0 +1,13 @@
+function Warning() {
+    return (
+        <div className="jumpscare-background">
+            <div className="jumpscare-warning">
+                <p>WARNING!</p>
+                <br />
+                <p>This game contains flashing lights, loud noises, and lots of jumpscares!</p>
+            </div>
+        </div>
+    );
+}
+
+export default Warning
