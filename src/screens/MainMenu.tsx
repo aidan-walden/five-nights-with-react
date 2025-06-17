6e6a7a8d7fdb06c1efa5f5021c2e428ddd65815b
Aidan Walden <44129920+aidan-walden@users.noreply.github.com>
2025-06-17 14:40:58 -0400

    Main menu looks good now

--- src/screens/MainMenu.tsx
+++ src/screens/MainMenu.tsx
@@ -5,30 +5,51 @@ function MainMenu() {
     const [backgroundImage, setBackgroundImage] = useState("https://i.imgur.com/1234567890.jpg");
 
     useEffect(() => {
+        const BASE_IMAGE = "/fnaf_assets/mainmenu/bg/0.png";
         const backgroundImages = [
-            "/fnaf_assets/mainmenu/bg/0.png",
             "/fnaf_assets/mainmenu/bg/1.png",
             "/fnaf_assets/mainmenu/bg/2.png",
             "/fnaf_assets/mainmenu/bg/3.png",
         ];
         
-        const randomIndex = Math.floor(Math.random() * backgroundImages.length);
-        setBackgroundImage(backgroundImages[randomIndex]);
+        setBackgroundImage(BASE_IMAGE);
+
+        function changeBackground() {
+            const randomIndex = Math.floor(Math.random() * backgroundImages.length);
+            setTimeout(() => {
+                setBackgroundImage(backgroundImages[randomIndex]);
+    
+    
+                // 1000 * (2/15) because FNaF displays the alternate image for 4 frames out of 30fps
+                setTimeout(() => {
+                    setBackgroundImage(BASE_IMAGE);
+                }, 1000 * (2/15));
+
+                changeBackground();
+            }, 8000);
+        }
+
+        changeBackground();
     }, []);
 
     return (
         <div className="full-background" style={{ backgroundImage: `url(${backgroundImage})` }}>
+            <video className="main-menu-video" src="/fnaf_assets/mainmenu/bg/static.webm" autoPlay loop muted controls={false}/>
             <div className="main-menu-container">
-                <div className="main-menu-title">
-                    <h1>Five</h1>
-                    <h1>Nights</h1>
-                    <h1>at</h1>
-                    <h1>Freddy's</h1>
+                <div className="main-menu-text">
+                    <p>Five</p>
+                    <p>Nights</p>
+                    <p>at</p>
+                    <p>Freddy's</p>
                 </div>
                 <div className="main-menu-buttons">
-                    <button>Play</button>
-                    <button>Options</button>
+                    <button>New Game</button>
+                    <button>Continue</button>
                 </div>
+            </div>
+            <div className="main-menu-text footer">
+                <p>© 2014 Scott Cawthon</p>
+                <p>Website by Aidan Walden</p>
             </div>
         </div>
     )
