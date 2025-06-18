23966430a66b15fc35f76634d8d6ad163143bfc7
Aidan Walden <44129920+aidan-walden@users.noreply.github.com>
2025-06-18 15:10:00 -0400

    Partially functional new game button

--- src/screens/MainMenu.tsx
+++ src/screens/MainMenu.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 function MainMenu() {
 
-    const [backgroundImage, setBackgroundImage] = useState("https://i.imgur.com/1234567890.jpg");
+    const [backgroundImage, setBackgroundImage] = useState("/fnaf_assets/mainmenu/bg/0.png");
 
     useEffect(() => {
         const BASE_IMAGE = "/fnaf_assets/mainmenu/bg/0.png";
@@ -32,26 +32,46 @@ function MainMenu() {
         changeBackground();
     }, []);
 
+    const newGame = () => {
+        const buttonsDiv = document.querySelector(".main-menu-buttons");
+        const buttons = buttonsDiv?.querySelectorAll("button");
+        buttons?.forEach((button: HTMLButtonElement) => {
+            button.disabled = true;
+        });
+
+        const menu = document.querySelector(".full-background");
+        menu?.classList.add("fade-out");
+
+        const newspaper = document.querySelector(".newspaper-ad");
+        newspaper?.classList.add("fade-in");
+        (newspaper as HTMLElement).style.visibility = "visible";
+    }
+
     return (
-        <div className="full-background" style={{ backgroundImage: `url(${backgroundImage})` }}>
-            <video className="main-menu-video" src="/fnaf_assets/mainmenu/bg/static.webm" autoPlay loop muted controls={false}/>
-            <div className="main-menu-container">
-                <div className="main-menu-text">
-                    <p>Five</p>
-                    <p>Nights</p>
-                    <p>at</p>
-                    <p>Freddy's</p>
+        <>
+            <div className="full-background" style={{ backgroundImage: `url(${backgroundImage})` }}>
+                <video className="main-menu-video" src="/fnaf_assets/mainmenu/bg/static.webm" autoPlay loop muted controls={false}/>
+                <audio src="/fnaf_assets/mainmenu/music/music.wav" autoPlay loop controls={false}/>
+                <div className="main-menu-container">
+                    <div className="main-menu-text">
+                        <p>Five</p>
+                        <p>Nights</p>
+                        <p>at</p>
+                        <p>Freddy's</p>
+                    </div>
+                    <div className="main-menu-buttons">
+                        <button onClick={newGame}>New Game</button>
+                        <button>Continue</button>
+                    </div>
                 </div>
-                <div className="main-menu-buttons">
-                    <button>New Game</button>
-                    <button>Continue</button>
+                <div className="main-menu-text footer">
+                    <p>© 2014 Scott Cawthon</p>
+                    <p>Website by Aidan Walden</p>
                 </div>
             </div>
-            <div className="main-menu-text footer">
-                <p>© 2014 Scott Cawthon</p>
-                <p>Website by Aidan Walden</p>
+            <div className="full-background newspaper-ad" style={{ backgroundImage: "url(/fnaf_assets/mainmenu/bg/newspaper.png)" }}>
             </div>
-        </div>
+        </>
     )
 }
 
